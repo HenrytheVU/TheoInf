4ace8929b4fbbfb4b8bad714b2fcf3b52a425c52
Henry Vu <HenrytheVU@gmail.com>
2015-05-06 09:19:30 +0200

    Dijkstra in progress

--- TheoInf/src/aufgabe1/Dijkstra.java
+++ TheoInf/src/aufgabe1/Dijkstra.java
@@ -1,12 +1,19 @@
 package aufgabe1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jgrapht.Graph;
 
 public class Dijkstra  {
 
 	public static <V, E> void findShortestPath(Graph<V, E> graph, V startVertex, V endVertex) {
+		List<String> allVertexes = new ArrayList<>();
+		List<String> visitedVertexes = new ArrayList<>();
 		for(V v : graph.vertexSet()){
 			System.out.println(v);
+			allVertexes.add(v.toString());
 		}
 
 	}
